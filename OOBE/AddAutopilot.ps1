0e98b3e3af4f4434f0a567ea942859d30fad114f
Ronny Roethof <ronny@roethof.net>
2023-08-03 06:26:58 +0200

    test

--- OOBE/AddAutopilot.ps1
+++ OOBE/AddAutopilot.ps1
@@ -1,19 +1,4 @@
-$IntuneModule = Get-Module -Name "Microsoft.Graph.Intune" -ListAvailable
-if (!$IntuneModule){
-write-host "Microsoft.Graph.Intune Powershell module not installed..." -f Red
-write-host "Install by running 'Install-Module Microsoft.Graph.Intune' from an elevated PowerShell prompt" -f Yellow
-write-host "Script can't continue..." -f Red
-write-host
-exit
-}
+iex (irm functions.osdcloud.com)
+osdcloud-InstallScriptAutopilot
 
-####################################################
-# Importing the SDK Module
-Import-Module -Name Microsoft.Graph.Intune
-if(!(Connect-MSGraph)){
-Connect-MSGraph
-}
-####################################################
-
-Install-Script Get-Windowsautopilotinfo
 Get-WindowsAutoppilotinfo -online
