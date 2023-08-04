63e5af109d64131079ad2341fc11dfda9f6c4bc1
Ronny Roethof <ronny@roethof.net>
2023-08-04 18:22:41 +0200

    Update AddAutopilot.ps1

--- OOBE/AddAutopilot.ps1
+++ OOBE/AddAutopilot.ps1
@@ -6,7 +6,7 @@ Install-Script -Name Get-WindowsAutoPilotInfo -Force
 Write-Host 'Installing dependencies (Module: WindowsAutopilotIntune).'`n
 Write-Host 'Opening Login Window after the installation was successfull:'`n
  
-.\Get-WindowsAutopilotInfo.ps1 -online
+Get-WindowsAutopilotInfo.ps1 -online
          
 Write-Host 'Everything completed. Rebooting now ...'`n
 Start-Sleep -s 2
