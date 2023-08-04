a39546dd3a59cc7f684ed8156b386a9887916fc9
Ronny Roethof <ronny@roethof.net>
2023-08-04 18:20:28 +0200

    Update AddAutopilot.ps1

--- OOBE/AddAutopilot.ps1
+++ OOBE/AddAutopilot.ps1
@@ -1,5 +1,13 @@
-Set-Executionpolicy RemoteSigned
-Install-Script Get-WindowsAutopilotImportGUI -Force -SkipPublisherCheck
-Get-WindowsAutopilotImportGUI
-
-#Restart-Computer
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
+Install-PackageProvider -Name NuGet -Force
+Write-Host 'Installing Get-WindowsAutopilotInfo:'`n
+Install-Script -Name Get-WindowsAutoPilotInfo -Force
+  
+Write-Host 'Installing dependencies (Module: WindowsAutopilotIntune).'`n
+Write-Host 'Opening Login Window after the installation was successfull:'`n
+ 
+.\Get-WindowsAutopilotInfo.ps1 -online
+         
+Write-Host 'Everything completed. Rebooting now ...'`n
+Start-Sleep -s 2
+Restart-Computer
