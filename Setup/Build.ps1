b2a364d8b1be5438bd29f51a491b0f9b389f4bbb
Ronny Roethof <ronny@roethof.net>
2023-10-30 13:09:15 +0100

    Update Build.ps1

--- Setup/Build.ps1
+++ Setup/Build.ps1
@@ -12,10 +12,7 @@ New-OSDCloudTemplate -Language nl-nl -SetInputLocale nl-nl -Verbose
 $WorkingDir="C:\OSDCloud"
 New-OSDCloudWorkspace -WorkspacePath $WorkingDir -Verbose
 
-New-Item -ItemType Directory $WorkingDir\Media\OSDCloud\Automate
-Copy-Item .\AutoPilotConfigurationFile.json $WorkingDir\Media\OSDCloud\Automate 
-
-Invoke-WebRequest https://raw.githubusercontent.com/AnyLinQ-B-V/osdcloud/main/Media/wallpaper.jpg -OutFile $WorkingDir\Media\wallpaper.jpg
+Invoke-WebRequest https://raw.githubusercontent.com/AnyLinQ-B-V/osdcloud/main/Media/wallpaper.jpg -OutFile C:\OSDCloud\Media\wallpaper.jpg
 
 Edit-OSDCloudWinPE -WorkspacePath $WorkingDir
 Edit-OSDCloudWinPE -CloudDriver Dell,HP,IntelNet,LenovoDock,USB,VMware,WiFi -StartURL https://raw.githubusercontent.com/AnyLinQ-B-V/osdcloud/main/OOBE/W11_OOBEcmd.ps1 -wallpaper $WorkingDir\Media\wallpaper.jpg -Verbose
